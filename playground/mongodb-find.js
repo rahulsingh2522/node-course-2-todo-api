622ab61ac02dcc64b50ced96fc5d361368f0420a
rahul.singh@salesforce.com <Rahul Singh>
2017-09-03 23:57:51 -0400

    Init

---
+++ playground/mongodb-find.js
@@ -0,0 +1,35 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb'); //ES 6 Destructuring
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('59acc6427bdb4407f9e7a3c8')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);       
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({
+        name: 'Rahul'
+    }).toArray().then((docs) => {
+        console.log('Users');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch Users', err);
+    });
+
+    //db.close();
+});
