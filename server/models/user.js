8728dd6c7b81b22b87f7de812e0333b248d1f539
rahul.singh@salesforce.com <Rahul Singh>
2017-09-14 13:50:25 -0400

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -32,6 +32,7 @@ var UserSchema = new mongoose.Schema({
     }]    
 });
 
+//To set instance methods
 UserSchema.methods.toJSON = function () {
     var user = this;
     var userObject = user.toObject();
@@ -49,6 +50,24 @@ UserSchema.methods.generateAuthToken = function () {
     return user.save().then(() => {
         return token;
     })
+};
+
+//To set Model methods
+UserSchema.statics.findByToken = function(token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        return Promise.reject();
+    }
+    
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
 };
 
 var User = mongoose.model('User', UserSchema);
