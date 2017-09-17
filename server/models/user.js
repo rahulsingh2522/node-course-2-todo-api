8a103012d36c82a7cddcb49807b8d3f29c765799
rahul.singh@salesforce.com <Rahul Singh>
2017-09-17 00:49:04 -0400

    Add DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -53,6 +53,16 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+
+   return user.update({
+        $pull: {
+            tokens: {token}
+        }
+    });
+};
+
 //To set Model methods
 UserSchema.statics.findByToken = function(token) {
     var User = this;
